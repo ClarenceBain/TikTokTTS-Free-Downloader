ad89b975fd593bdf3c8b74193b200571450f529e
Clarence Bain <51936921+ClarenceBain@users.noreply.github.com>
2024-08-02 05:07:50 -0400

    Cleaned up code, simple errors & interval abort

--- content.js
+++ content.js
@@ -1,47 +1,46 @@
+let abort = 0
+
 function findGap3() {
+	if (abort >= 5) { console.error("Aborted."); clearInterval(interval); return; }
+	
 	const home = document.querySelector("div.grow");
-	if(home !== null) {
-		console.log("Static region found: ", home);
-		
-		const free = document.createElement("button");
-		free.textContent = "Free Download";
-		free.addEventListener("click", () => {
-			const group = document.querySelector("div.flex.gap-3");
-			if (group !== null) {
-				console.log("Button group found: ", group);
-				const download = group.querySelector("a");
-				if(download !== null) {
-					console.log("Download button found: ", download);
-					const file = download.attributes[0].nodeValue;
-					const file_data64 = file.split(",")[1];	
-					const raw_data = atob(file_data64);
+	const group = document.querySelector("div.flex.gap-3");
+	
+	if (home == null || group == null) { abort++; console.error("Error: 0x1"); return; }
+	
+	const download = group.querySelector("a"); 
+	const file = download.getAttribute("href");
+	
+	if (download == null || file == null) {	abort++; console.error("Error: 0x2"); return; } 
+	
+	console.log("Static region found: ", home);
+	console.log("Button group found: ", group);
+	console.log("Download button found: ", download);
+	
+	const free = document.createElement("button");
+	free.textContent = "Free Download";
+	free.addEventListener("click", () => {
+		const file_data64 = file.split(",")[1];
+		const raw_data = atob(file_data64);
 					
-					const bytes = new Array(raw_data.length);
-					for (let i = 0; i < raw_data.length; i++) {
-						bytes[i] = raw_data.charCodeAt(i);
-					}
+		const bytes = new Array(raw_data.length);
+		for (let i = 0; i < raw_data.length; i++) {
+			bytes[i] = raw_data.charCodeAt(i);
+		}
 					
-					const data = new Uint8Array(bytes);
-					const blob = new Blob([data], { type: "audio/mp3" });
+		const data = new Uint8Array(bytes);
+		const blob = new Blob([data], { type: "audio/mp3" });
 					
-					const mp3 = document.createElement("a");
-					mp3.href = URL.createObjectURL(blob);
-					mp3.download = "tiktok-text-to-speech.mp3";
-					mp3.click();
+		const mp3 = document.createElement("a");
+		mp3.href = URL.createObjectURL(blob);
+		mp3.download = "tiktok-text-to-speech.mp3";
+		mp3.click();
 					
-					URL.revokeObjectURL(mp3.href);
-				} else {
-					console.error("Download button not found..");
-				}
-			} else {
-				console.error("Button group not found..");
-			}
-		});
-		home.appendChild(free);
-		clearInterval(interval);
-	} else {
-		console.warn("Looking for static region..");
-	}
+		URL.revokeObjectURL(mp3.href);		
+	});
+	
+	home.appendChild(free);
+	clearInterval(interval);
 }
 
-const interval = setInterval(findGap3, 500);
+const interval = setInterval(findGap3, 200);
